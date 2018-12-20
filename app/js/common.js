4edc64eda9c8eb76de868ad837f09485375c7e6a
Vladimir Kachanov <mykachanov@yandex.ru>
2018-12-20 08:48:36 +0300

    ответ + фикс

--- app/js/common.js
+++ app/js/common.js
@@ -1,44 +1,83 @@
-var typed = new Typed('#typed', {
-   strings: ['Workshop', 'Conference'],
-   typeSpeed: 40,
-   backSpeed: 40,
-   backDelay: 1700,//Задержка текста перед стиранием
-   loop: true, //цикл сообщений
-   showCursor: false
- });
+const baseWord = {
+  1:{
+    rus: 'Около \ Приблизительно',
+    eng: "About"
+  },
+  2:{
+    rus: 'Ему',
+    eng: "Him"
+  },
+  3:{
+    rus: 'Наследовать / Унаследовать / Перенять',
+    eng: "Inherit"
+  },
+}
 
- $(document).ready(function(){
-   $(".header__top").removeClass("header__top-active");
-   $(".header__top-logo").addClass("header__top-logo-first").fadeIn('fast');
-   $(window).scroll(function(){
-     if ($(this).scrollTop() > 40) {
-       $(".header__top").addClass("header__top-active").fadeIn('fast');
-       $(".header__top-logo").addClass("header__top-logo-second").fadeIn('fast');
-       $(".header__top-logo").removeClass("header__top-logo-first").fadeIn('fast');
-     } else {
-       $(".header__top").removeClass("header__top-active").fadeIn('fast');
-        $(".header__top-logo").removeClass("header__top-logo-second").fadeIn('fast');
-        $(".header__top-logo").addClass("header__top-logo-first").fadeIn('fast');
-     };
-   });
-
-   /// Всплывание блока информации и Спикерах + значек
-   $('.spaker-visible').on('click', function(){
-    let panelBox = $(this).parent().parent().parent();
-    panelBox.find('.speaker-info-person').toggle('');
-    $(this).parent().parent().find('.conference__event-cursor-active').toggleClass('conference__event-cursor-active-two');
-  });
-  /// Всплывание блока информации и Спикерах + значек
-
-  $('.nav_visible').on('click', function(){
-    let panelBox = $(this).parent().parent().parent().parent();
-    panelBox.find('.conference__content').toggle('');
-  });
+//глобаьная перменная
+window.rndValue = 0;
 
 
+// Стартовая генерация данных
+getRandomInt();
+formFill();
 
 
+/// Кнопка отправить
+toSend.onclick = function(){
+  working();
+}
+
+// Событие при нажатии на Enter
+document.onkeypress = function(){
+  if(event.keyCode == 13 ){
+    working();
+  }
+}
+
+/// Кнопка подсказка
+toHint.onclick = function(){
+  alert("wer");
+}
+
+function working (){
+  let sendWord = document.getElementById('sendWord').value;
+  if(sendWord == ""){
+    document.getElementById('result').innerHTML = "Ввели неверно, еще пробуйте";
+    document.getElementById('result').style.color = "red";
+    return;
+  }
+  sendWord = sendWord[0].toUpperCase() + sendWord.slice(1);
+  if (sendWord != baseWord[rndValue].eng){
+    document.getElementById('result').innerHTML = "Ввели неверно, еще пробуйте";
+    document.getElementById('result').style.color = "red";
+  }else{
+    document.getElementById('result').innerHTML = "Был правильный ответ";
+    document.getElementById('result').style.color = "#11b721";
+    setTimeout(function(){     
+      document.getElementById('sendWord').value = "";
+      document.getElementById('result').innerHTML = "&nbsp;";
+      getRandomInt();
+      formFill();
+    } , 1000);
+  }
+}
 
 
+// Заполнение полей после загрузки и нажатия
+function formFill(){
+  let rusWord = baseWord[rndValue].rus;
+  let baseWordRus = document.getElementById('baseWordRus').innerHTML = rusWord;
+  /// создать переменную getRandomInt
+}
 
- });
+// Случайное числа От мин в оббъекте до макс.
+function getRandomInt() {
+  let count  = 0;
+  for(key in baseWord){
+    count++;
+  }
+  let sumBaseWord = 1 - 0.5 + Math.random() * (count - 1 + 1)
+    sumBaseWord = Math.round(sumBaseWord);
+    rndValue = sumBaseWord;
+}
+
